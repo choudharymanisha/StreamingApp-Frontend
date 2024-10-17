de7bb9a284812f62db0b10c8249cfc1d4bdc3c37
choudharymanisha <choudharymanisha38566@gmail.com>
2024-10-17 23:51:29 +0530

    Updated file input handling in VideoUpload component

--- src/Components/VideoUpload.jsx
+++ src/Components/VideoUpload.jsx
@@ -1,39 +1,52 @@
-import React from "react";
-import videoLogo from "../assets/upload.png"
-import { Button, Card } from "flowbite-react";
+import React, { useState } from "react";
+import videoLogo from "../assets/upload.png";
+import { Button, Card, Label, TextInput } from "flowbite-react";
 
-function VideoUpload(){
-    return(
-        <>
-        <div className="text-white"> 
-       <Card className="flex flex-col items-center justify-center">
-        <div>
-        <h1> Upload Videos</h1>
-        <form class="flex items-center space-x-6">
-  <div class="shrink-0">
-    <img class="h-16 w-16 object-cover rounded-full" src={videoLogo} alt="Current profile photo" />
-  </div>
-  <label class="block">
-    <span class="sr-only">Choose profile photo</span>
-    <input type="file" class="block w-full text-sm text-slate-500
-      file:mr-4 file:py-2 file:px-4
-      file:rounded-full file:border-0
-      file:text-sm file:font-semibold
-      file:bg-violet-50 file:text-violet-700
-      hover:file:bg-violet-100
-    "/>
-  </label>
+function VideoUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [uploadFile, setUploadFile] = useState(false);
+  const [message, setMessage] = useState("");
 
-</form>
-<div className="flex justify-center">
-    <Button> Upload</Button>
-  </div>
-</div>
-       </Card>
-        </div>
-        
-        </>
-    )
-   
+  function handleFileChange(event) {
+    console.log(event.target.files[0]);
+  }
+
+  function handleForm(formEvent) {
+    console.log("button clicked");
+  }
+
+  return (
+    <>
+      <div className="text-white">
+        <Card className="flex flex-col items-center justify-center">
+          <div className="">
+            <h1>Upload Videos</h1>
+            <form className = "space-y-5" onSubmit={handleForm}>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="file-upload" value="Upload file" />
+                </div>
+                <TextInput placeholder="Enter title"/>
+              </div>
+              <div className="flex items-center space-x-5 justify-center">
+                <div className="shrink-0">
+                  <img className="h-16 w-16 object-cover rounded-full" src={videoLogo} alt="Current profile photo" />
+                </div>
+                <label className="block">
+                  <span className="sr-only">Choose profile photo</span>
+                  <input onChange={handleFileChange} type="file" className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" />
+                </label>
+              </div>
+            </form>
+            <div className="flex justify-center">
+              <Button>Upload</Button>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
 }
-export default VideoUpload
+
+export default VideoUpload;
