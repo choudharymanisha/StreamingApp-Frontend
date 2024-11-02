fb46f1e41c6a3e7ad7c14a9292ec135b7bcd6c97
choudharymanisha <choudharymanisha38566@gmail.com>
2024-11-02 23:09:49 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <div  className="flex flex-col items-center space-y-5 justify-center py-9">
-      <h1 className= "text-5xl font-extrabold text-gray-700 dark:text-gray-100"> Welcome to Video Streaming Application</h1>
+      <h1 className= "text-2xl font-bold text-gray-700 dark:text-gray-100">  Video Streaming Application</h1>
       <VideoUpload/>
       </div>
     </>
--- src/Components/VideoUpload.jsx
+++ src/Components/VideoUpload.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import videoLogo from "../assets/upload.png";
-import { Button, Card, Label, TextInput } from "flowbite-react";
+import { Button, Card, Label, TextInput,Textarea } from "flowbite-react";
 
 function VideoUpload() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [progress, setProgress] = useState(0);
   const [uploadFile, setUploadFile] = useState(false);
+  const[meta,setMeta] = useState({
+    title:"",
+    description:"",
+  });
   const [message, setMessage] = useState("");
 
   function handleFileChange(event) {
@@ -13,35 +17,43 @@ function VideoUpload() {
   }
 
   function handleForm(formEvent) {
-    console.log("button clicked");
+    formEvent.preventDefault();
+    console.log(formEvent);
   }
 
   return (
     <>
       <div className="text-white">
         <Card className="flex flex-col items-center justify-center">
-          <div className="">
+          <div className="flex flex-col space-y-6">
             <h1>Upload Videos</h1>
-            <form className = "space-y-5" onSubmit={handleForm}>
+            <form  noValidate className = "space-y-5" onSubmit={handleForm}>
               <div>
                 <div className="mb-2 block">
-                  <Label htmlFor="file-upload" value="Upload file" />
+                  <Label htmlFor="file-upload" value="upload file" />
                 </div>
-                <TextInput placeholder="Enter title"/>
+                <TextInput name = "title" placeholder="Enter title"/>
               </div>
+              <div className="max-w-md">
+      <div className="mb-2 block">
+        <Label htmlFor="comment" value="Video description" />
+      </div>
+      <Textarea name = "description " id="comment" placeholder="  Write video description...." required rows={4} />
+    </div>
               <div className="flex items-center space-x-5 justify-center">
                 <div className="shrink-0">
                   <img className="h-16 w-16 object-cover rounded-full" src={videoLogo} alt="Current profile photo" />
                 </div>
                 <label className="block">
                   <span className="sr-only">Choose profile photo</span>
-                  <input onChange={handleFileChange} type="file" className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" />
+                  <input name = "file" onChange={handleFileChange} type="file" className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" />
                 </label>
               </div>
-            </form>
-            <div className="flex justify-center">
-              <Button>Upload</Button>
+              <div className="flex justify-center">
+              <Button type = "submit">Submit</Button>
             </div>
+            </form>
+           
           </div>
         </Card>
       </div>
