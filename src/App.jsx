c37ba1b45acfc2c5a009da252f50c4329a2960b7
Manisha <choudharymanisha38566@gmail.com>
2024-11-26 21:35:08 +0530

    final features added

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import VideoUpload from './Components/VideoUpload'
-import { Toaster } from 'react-hot-toast'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import VideoUpload from "./Components/VideoUpload";
+import { Toaster } from "react-hot-toast";
+import VideoPlayer from "./Components/VideoPlayer";
+import { Button, TextInput } from "flowbite-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const[videoId,setVideoId] = useState("1bf1f5fd-141a-4320-b31b-0d78fb3ddcd5",)
+  const [count, setCount] = useState(0);
+  const [fieldValue, setFieldValue] = useState(null);
+  const [videoId, setVideoId] = useState(
+    "084565fb-7957-40a6-9322-0151319180e9"
+  );
 
+  function playVideo(videoId) {
+    setVideoId(videoId);
+  }
   return (
     <>
-    <Toaster/>
-    <div  className="flex flex-col items-center space-y-5 justify-center py-9">
-      <h1 className= "text-2xl font-bold text-gray-700 dark:text-gray-100">  Video Streaming Application</h1>
-      <div className='flex mt-14 w-full justify-around'>
-      <div>
-        <h1 className='text-white'> Playing Video in straimg app </h1>
-        <video
-        style = {{
-          width:500,
-          height:500,
-        }}
-        src = {`http://localhost:8080/api/v1/videos/stream/range/${videoId}`} controls
-        />
-      </div>
-      
-      <VideoUpload/>
-      </div>
+      <Toaster />
+      <div className="flex flex-col  items-center space-y-9 justify-center py-9">
+        <h1 className="text-2xl font-bold text-gray-700 dark:text-gray-100">
+          Video Streaming App
+        </h1>
+
+        <div className="flex mt-14 w-full space-x-2  justify-between">
+          <div className="w-full">
+            <h1 className="text-white text-center mt-2">Playing Video</h1>
+
+            {/* <video
+              style={{
+                width: "100%",
+              }}
+              // src={`http://localhost:8080/api/v1/videos/stream/range/${videoId}`}
+              src="http://localhost:8080/api/v1/videos/f6e21144-c462-459b-af96-1cde95621710/master.m3u8"
+              controls
+            ></video> */}
+
+            <div>
+              <VideoPlayer
+                src={`http://localhost:8080/api/v1/videos/${videoId}/master.m3u8`}
+              ></VideoPlayer>
+            </div>
+
+            {/* <video
+              id="my-video"
+              class="video-js"
+              controls
+              preload="auto"
+              width="640"
+              height="264"
+              data-setup="{}"
+            >
+              <source
+                src={`http://localhost:8080/api/v1/videos/stream/range/${videoId}`}
+                type="video/mp4"
+              />
+
+              <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider
+                upgrading to a web browser that
+                <a
+                  href="https://videojs.com/html5-video-support/"
+                  target="_blank"
+                >
+                  supports HTML5 video
+                </a>
+              </p>
+            </video>  */}
+          </div>
+
+          <div className="w-full">
+            <VideoUpload />
+          </div>
+        </div>
+
+        <div className="my-4 flex  space-x-4">
+          <TextInput
+            onClick={(event) => {
+              setFieldValue(event.target.value);
+            }}
+            placeholder="Enter video id here"
+            name="video_id_field"
+          />
+          <Button
+            onClick={() => {
+              setVideoId(fieldValue);
+            }}
+          >
+            Play
+          </Button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/VideoUpload.jsx
+++ src/Components/VideoUpload.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
 import videoLogo from "../assets/upload.png";
-import { Alert, Button, Card, Label, Progress, TextInput, Textarea } from "flowbite-react";
+import {
+  Button,
+  Card,
+  Label,
+  FileInput,
+  TextInput,
+  Textarea,
+  Progress,
+  Alert,
+} from "flowbite-react";
 import axios from "axios";
 import toast from "react-hot-toast";
-
 function VideoUpload() {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [progress, setProgress] = useState(0);
-  const [uploading, setUploading] = useState(false);  // Define uploading state
   const [meta, setMeta] = useState({
     title: "",
     description: "",
   });
+  const [progress, setProgress] = useState(0);
+  const [uploading, setUploading] = useState(false);
   const [message, setMessage] = useState("");
 
   function handleFileChange(event) {
+    console.log(event.target.files[0]);
     setSelectedFile(event.target.files[0]);
   }
 
   function formFieldChange(event) {
+    // console.log(event.target.name);
+    // console.log(event.target.value);
     setMeta({
       ...meta,
       [event.target.name]: event.target.value,
@@ -28,102 +39,164 @@ function VideoUpload() {
   function handleForm(formEvent) {
     formEvent.preventDefault();
     if (!selectedFile) {
-      alert("Select a file, please");
+      alert("Select File !!");
       return;
     }
+    //submit the file to server:
     saveVideoToServer(selectedFile, meta);
   }
-  function resetForm(){
+
+  function resetForm() {
     setMeta({
-      title:"",
-      description:"",
-    })
+      title: "",
+      description: "",
+    });
     setSelectedFile(null);
     setUploading(false);
+    // setMessage("");
   }
 
+  //submit file to server
   async function saveVideoToServer(video, videoMetaData) {
     setUploading(true);
+
+    //api call
+
     try {
       let formData = new FormData();
       formData.append("title", videoMetaData.title);
       formData.append("description", videoMetaData.description);
-      formData.append("file", video);
+      formData.append("file", selectedFile);
 
-      let response = await axios.post(`http://localhost:8080/api/v1/videos`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        onUploadProgress: (progressEvent) => {
-          const progress = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-          setProgress(progress);
-        },
-      });
+      let response = await axios.post(
+        `http://localhost:8080/api/v1/videos`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (progressEvent) => {
+            const progress = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+
+            console.log(progress);
+            setProgress(progress);
+          },
+        }
+      );
+
+      console.log(response);
       setProgress(0);
 
-      setMessage("File uploaded successfully!");
+      setMessage("File uploaded " + response.data.videoId);
       setUploading(false);
-      toast.success("File uploaded successfully");
+      toast.success("File uploaded successfully !!");
       resetForm();
     } catch (error) {
-      console.log("Error in uploading:", error);
+      console.log(error);
+      setMessage("Error in uplaoding File");
       setUploading(false);
-      toast.error("Error in file uploading");
+      toast.error("File not uploaded !!");
     }
   }
 
   return (
     <div className="text-white">
       <Card className="flex flex-col items-center justify-center">
-        <div className="flex flex-col space-y-6">
-          <h1>Upload Videos</h1>
-          <form noValidate className="space-y-5" onSubmit={handleForm}>
+        <h1>Upload Videos</h1>
+
+        <div>
+          <form
+            noValidate
+            className=" flex flex-col space-y-6"
+            onSubmit={handleForm}
+          >
             <div>
-              <Label htmlFor="file-upload" value="Upload file" />
-              <TextInput  value = {meta.title}onChange={formFieldChange} name="title" placeholder="Enter title" />
+              <div className="mb-2 block">
+                <Label htmlFor="file-upload" value="Video Title" />
+              </div>
+              <TextInput
+                value={meta.title}
+                onChange={formFieldChange}
+                name="title"
+                placeholder="Enter title"
+              />
             </div>
+
             <div className="max-w-md">
-              <Label htmlFor="comment" value="Video description" />
+              <div className="mb-2 block">
+                <Label htmlFor="comment" value="Video Description" />
+              </div>
               <Textarea
-              value = {meta.description}
+                value={meta.description}
                 onChange={formFieldChange}
                 name="description"
                 id="comment"
-                placeholder="Write video description... here "
+                placeholder="Write video description..."
                 required
                 rows={4}
               />
             </div>
+
             <div className="flex items-center space-x-5 justify-center">
-              <img className="h-16 w-16 object-cover rounded-full" src={videoLogo} alt="Upload" />
-              <input
-                name="file"
-                onChange={handleFileChange}
-                type="file"
-                className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
-              />
+              <div className="shrink-0">
+                <img
+                  className="h-16 w-16 object-cover "
+                  src={videoLogo}
+                  alt="Current profile photo"
+                />
+              </div>
+              <label className="block">
+                <span className="sr-only">Choose video file</span>
+                <input
+                  name="file"
+                  onChange={handleFileChange}
+                  type="file"
+                  className="block w-full text-sm text-slate-500
+      file:mr-4 file:py-2 file:px-4
+      file:rounded-full file:border-0s
+      file:text-sm file:font-semibold
+      file:bg-violet-50 file:text-violet-700
+      hover:file:bg-violet-100
+    "
+                />
+              </label>
             </div>
-            <Progress
-              hidden={!uploading}
-              progress={progress}
-              textLabel="Uploading"
-              size="lg"
-            />
-            {message && (
-              <Alert color={"success"}
-              rounded 
-              withBorderAccent
-              onDismiss={() => {
-                setMessage("")
-              }}
 
+            <div className="">
+              {uploading && (
+                <Progress
+                  color="green"
+                  progress={progress}
+                  textLabel="Uploading"
+                  size={"lg"}
+                  labelProgress
+                  labelText
+                />
+              )}
+            </div>
 
-              >
-                <span className="font-medium">{message}</span>
-              </Alert>
-            )}
+            <div className="">
+              {message && (
+                <Alert
+                  color={"success"}
+                  rounded
+                  withBorderAccent
+                  onDismiss={() => {
+                    setMessage("");
+                  }}
+                >
+                  <span className="font-medium">Success alert! </span>
+                  {message}
+                </Alert>
+              )}
+            </div>
+
             <div className="flex justify-center">
-              <Button disabled = {uploading} type="submit">Submit</Button>
+              <Button disabled={uploading} type="submit">
+                Submit
+              </Button>
             </div>
           </form>
         </div>
